8b034fd6b566b868e8a72b7562071aba772b6040
Eric Williamson <ericrw96@vt.edu>
2021-04-22 22:49:04 -0700

    fix to localhost

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom"
 import './index.css';
 import App from './App';
 
-
-
 const root = document.getElementById("root") as HTMLElement;
 ReactDOM.unstable_createRoot(root).render(<App />);
